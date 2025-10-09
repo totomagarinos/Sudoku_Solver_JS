510811903465a602704b9acaaddfc1516166b2ce
Juan Cruz Magariños <totomagarinos@gmail.com>
2025-10-08 21:52:51 -0300

    feat: implement display solution and autocomplete

--- src/board.js
+++ src/board.js
@@ -37,4 +37,44 @@ function getBoardValues() {
   return boardArray;
 }
 
-export { createBoard, getBoardValues };
+function displaySolution(solvedBoard) {
+  const inputs = document.querySelectorAll(".cell");
+
+  inputs.forEach((input) => {
+    const row = parseInt(input.dataset.row);
+    const col = parseInt(input.dataset.col);
+    input.value = solvedBoard[row][col];
+  });
+
+  return solvedBoard;
+}
+
+function autoCompleteBoard() {
+  const defaultBoard = [
+    [5, 3, 0, 0, 7, 0, 0, 0, 0],
+    [6, 0, 0, 1, 9, 5, 0, 0, 0],
+    [0, 9, 8, 0, 0, 0, 0, 6, 0],
+    [8, 0, 0, 0, 6, 0, 0, 0, 3],
+    [4, 0, 0, 8, 0, 3, 0, 0, 1],
+    [7, 0, 0, 0, 2, 0, 0, 0, 6],
+    [0, 6, 0, 0, 0, 0, 2, 8, 0],
+    [0, 0, 0, 4, 1, 9, 0, 0, 5],
+    [0, 0, 0, 0, 8, 0, 0, 7, 9],
+  ];
+
+  const inputs = document.querySelectorAll(".cell");
+
+  inputs.forEach((input) => {
+    const row = input.dataset.row;
+    const col = input.dataset.col;
+    const value = defaultBoard[row][col];
+
+    if (value !== 0) {
+      input.value = value;
+    } else {
+      input.value = "";
+    }
+  });
+}
+
+export { createBoard, getBoardValues, displaySolution, autoCompleteBoard };
--- src/main.js
+++ src/main.js
@@ -1,3 +1,22 @@
-import { createBoard } from "./board";
+import {
+  createBoard,
+  getBoardValues,
+  displaySolution,
+  autoCompleteBoard,
+} from "./board";
+import { solveSudoku } from "./solver";
+
+document.getElementById("solve-button").addEventListener("click", () => {
+  const board = getBoardValues();
+  if (solveSudoku(board)) {
+    displaySolution(board);
+  } else {
+    console.log("No tiene solucion");
+  }
+});
+
+document
+  .getElementById("auto-complete-button")
+  .addEventListener("click", autoCompleteBoard);
 
 createBoard();
